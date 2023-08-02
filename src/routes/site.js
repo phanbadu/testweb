49fda694b54d73991ac5065adcf378502b63bd7d
phanbadu <duphan03@gmail.com>
2023-08-03 02:46:15 +0700

    first commit

---
+++ src/routes/site.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const SiteController = require('../app/controllers/SiteController');
+
+// newsController.index
+router.get('/search', SiteController.search)
+router.get('/', SiteController.index)
+
+module.exports = router;
