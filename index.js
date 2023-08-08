fdf720e3036de5a3dbe8baa99bd12267c84bb671
phanbadu <duphan03@gmail.com>
2023-08-09 04:30:52 +0700

    update code

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.use('/public', express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+    var file = path.join(__dirname, 'home.html');
+    res.sendFile(file);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
--- src/routes/me.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const meController = require('../app/controllers/MeController');
-
-// courseController.show
-router.get('/stored/courses', meController.storedCourses);
-
-module.exports = router;
