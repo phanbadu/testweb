de6f8a7c7ae762e17b1ee01ef42aae25d8705b07
phanbadu <119209610+phanbadu@users.noreply.github.com>
2023-08-11 18:02:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.get('/webhooks', (req, res) => {
     res.sendFile(file);
 });
 
+app.get('/', (req, res) => {
+    var file = path.join(__dirname, 'home.html');
+    res.sendFile(file);
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
